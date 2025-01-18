2f1d49d099b960e4f92248f154aa3a101f9d0478
Brahim Bouchikha <Brahim.Bouchikha@gmail.com>
2025-01-18 21:56:58 +0100

    update

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,8 +1,8 @@
 /**
- * Retrieves a list of students.
- * @author Bezaleel Olakunori <https://github.com/brahimbouchikha/>
- * @returns {{id: Number, firstName: String, location: String}[]}
- */
+* Retrieves a list of students.
+* @author Bezaleel Olakunori <https://github.com/B3zaleel>
+* @returns {{id: Number, firstName: String, location: String}[]}
+*/
 export default function getListStudents() {
   return [
     { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
